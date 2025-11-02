54c11b6c9ac213587dfe6c4e6cb4fbf1e1da5df2
DarrylAdigwu <115912161+DarrylAdigwu@users.noreply.github.com>
2025-11-01 21:40:14 -0700

    made changes to eslint.config and added a README

--- eslint.config.js
+++ eslint.config.js
@@ -2,6 +2,8 @@ import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import reactX from 'eslint-plugin-react-x'
+import reactDom from 'eslint-plugin-react-dom'
 import tseslint from 'typescript-eslint'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
@@ -12,12 +14,27 @@ export default defineConfig([
     extends: [
       js.configs.recommended,
       tseslint.configs.recommended,
+      // Remove tseslint.configs.recommended and replace with this
+      tseslint.configs.recommendedTypeChecked,
+      // Alternatively, use this for stricter rules
+      tseslint.configs.strictTypeChecked,
+      // Optionally, add this for stylistic rules
+      tseslint.configs.stylisticTypeChecked,
+      // Enable lint rules for React
+      reactX.configs['recommended-typescript'],
+      // Enable lint rules for React DOM
+      reactDom.configs.recommended,
       reactHooks.configs['recommended-latest'],
       reactRefresh.configs.vite,
     ],
     languageOptions: {
+      parserOptions: {
+          project: ["./tsconfig.node.json", "./tsconfig.app.json"], // Or your specific tsconfig files
+          tsconfigRootDir: import.meta.dirname,
+        },
       ecmaVersion: 2020,
       globals: globals.browser,
     },
   },
 ])
+
